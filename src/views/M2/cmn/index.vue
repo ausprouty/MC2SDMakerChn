327af6d05eb6d2f3a183a7f7b9bafd7aa444122e
ausprouty <bob.prouty@powertochange.org.au>
2023-01-06 16:18:31 +1100

    first commit

---
+++ src/views/M2/cmn/index.vue
@@ -0,0 +1,116 @@
+<script>
+import {useShare} from "@/assets/javascript/share.js"
+
+export default {
+  
+  methods:{
+    vuePush(id){
+      this.$router.push({
+        name: id,
+      })
+    },
+    share(what, v1, v2){
+      useShare(what, v1, v2)
+    }
+  },
+  mounted() {
+    localStorage.setItem("returnpage", this.$route.name)
+  }
+}
+</script>
+<template>
+  <!-- sdcard template from mc2 -->
+  
+  <div class="page_content" dir="ltr">
+    <div>
+      
+    </div>
+    <div>
+      <!-- begin mc2 sdcard bookImage -->
+<div class="Xapp-link">
+  <div class="card-link" @click="vuePush('cmn-hope-index')">
+    <div class="Xapp-card -Xshadow">
+      <img src="@/assets/images/cmn/standard/Hope.png" class="book-large" />
+    </div>
+  </div>
+</div>
+<!-- end mc2 sdcard bookImage -->
+<!-- begin mc2 sdcard bookImage -->
+<div class="Xapp-link">
+  <div class="card-link" @click="vuePush('cmn-prophet-index')">
+    <div class="Xapp-card -Xshadow">
+      <img src="@/assets/images/cmn/standard/Prophets.png" class="book-large" />
+    </div>
+  </div>
+</div>
+<!-- end mc2 sdcard bookImage -->
+<!-- begin mc2 sdcard bookImage -->
+<div class="Xapp-link">
+  <div class="card-link" @click="vuePush('cmn-multiply1-index')">
+    <div class="Xapp-card -Xshadow">
+      <img src="@/assets/images/cmn/standard/Multiply1.png" class="book-large" />
+    </div>
+  </div>
+</div>
+<!-- end mc2 sdcard bookImage -->
+<!-- begin mc2 sdcard bookImage -->
+<div class="Xapp-link">
+  <div class="card-link" @click="vuePush('cmn-multiply2-index')">
+    <div class="Xapp-card -Xshadow">
+      <img src="@/assets/images/cmn/standard/Multiply2.png" class="book-large" />
+    </div>
+  </div>
+</div>
+<!-- end mc2 sdcard bookImage -->
+<!-- begin mc2 sdcard bookImage -->
+<div class="Xapp-link">
+  <div class="card-link" @click="vuePush('cmn-multiply3-index')">
+    <div class="Xapp-card -Xshadow">
+      <img src="@/assets/images/cmn/standard/Multiply3.png" class="book-large" />
+    </div>
+  </div>
+</div>
+<!-- end mc2 sdcard bookImage -->
+<!-- begin mc2 sdcard bookImage -->
+<div class="Xapp-link">
+  <div class="card-link" @click="vuePush('cmn-tc-index')">
+    <div class="Xapp-card -Xshadow">
+      <img src="@/assets/images/cmn/standard/TransferableConcepts.png" class="book-large" />
+    </div>
+  </div>
+</div>
+<!-- end mc2 sdcard bookImage -->
+
+    </div>
+    <!-- begin mc2 sdcard languageFooter -->
+
+<div class="languages" id="languages"><img class="languages" src="@/assets/images/standard//OtherLanguagesTop.png" /></div>
+<table class="social">
+	<tbody>
+		<tr>
+			<td class="social" @click="share('languages', '', '')">
+				  <img class="social" src="@/assets/images/standard/languages.png" />
+			  </td>
+			  
+			<td class="social"  @click="share('android', 'cmn', '')">
+				<img  class="social" src="@/assets/images/standard/android.png" />
+			</td>
+
+			<td class="social" @click="share('lesson', 'library: ', '/content/M2/cmn/index.html')">
+				<img class="social" src="@/assets/images/standard/Share.png" />
+			</td>
+		</tr>
+	</tbody>
+</table>
+<div class="footer">
+<p class="footer">MC2</p>
+<p class="footer" @click="share('website', 'https://GlobalChurchMovements.org', '')"> GlobalChurchMovements.org</p>
+</div>
+
+<!-- end mc2 sdcard languageFooter -->
+</div>
+<!-- end default library -->
+</template>
+<!--- Created by publishLibrary-->
+<!-- begin sdcard Footer -->
+<!-- end sdcard Footer -->
